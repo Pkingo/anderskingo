b6b265bcd9ae81fc3991392db69b72542d9bcc98
PKingo <philipandreaskingo@hotmail.com>
2019-10-12 21:56:47 +0200

    Updated program

--- src/pages/kulturrejser/kulturrejser.tsx
+++ src/pages/kulturrejser/kulturrejser.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import Hellas from '../../assets/hellas1.jpg';
+import Hellas from '../../assets/hellas5.jpg';
 
 const Kulturrejser = () => (
   <div className="kulturrejser-container">
     <h1>Kulturrejse: Tilblivelsen af den europæiske kultur</h1>
     <div className="kulturrejse-left">
-      <h6>HELLAS 2018</h6>
+      <h6>HELLAS 2020</h6>
       <p>
-        Fra onsdag den 29. august til søndag den 9. september Elleve dage i vor
-        kulturs vugge rejseleder og arrangør Anders Kingo i samarbejde med
-        EUROLAND TRAVEL, Athen.
+        Fra lørdag den 6. juni til onsdag den 17. juni Tolv dage i vor kulturs
+        vugge rejseleder og arrangør Anders Kingo i samarbejde med EUROLAND
+        TRAVEL, Athen.
       </p>
       <p>
         Deltag i en uforglemmelig rejse til vores kulturs vugge, hvor vi følger
         og forfølger de to kulturer – den græske og den jødisk-kristne – der har
         skabt den europæiske kultur. Som optakt til turen afholder jeg en
-        studiekreds, hvor jeg gennemgår den græske mytologi og historie og nogle
-        hovedretninger inden for kunst, litteratur og filosofi. Også Paulus’
-        rejser til Filippi, Thessaloniki, Veria (Berøa), Korinth og Athen
-        skildres. Desuden foretager jeg en detaljeret gennemgang af turen.
+        studiekreds, i Helsingør og i Jylland (hvis interesse derfor), hvor jeg
+        gennemgår den græske mytologi og historie og nogle hovedretninger inden
+        for kunst, litteratur og filosofi. Også Paulus’ rejser til Filippi,
+        Thessaloniki, Veria (Berøa), Korinth og Athen skildres. Desuden
+        foretager jeg en detaljeret gennemgang af turen.
       </p>
       <p>
-        <strong>Pris:</strong> 14.700 kr. og inkluderer: <br />
+        <strong>Pris:</strong> 15.500 kr. og inkluderer: <br />
         Fly Kastrup-Thessaloniki og Athen-Kastrup. Bus, guidning og rejseleder.
         Samtlige entréer. Overnatning på et 5-stjernet og seks 4-stjernede
         hoteller inkl. morgenmad og aftensmad. Der overnattes på
-        dobbeltværelser. Tillæg for enkeltværelse 1.975 kr.
+        dobbeltværelser. Tillæg for enkeltværelse 3.500 kr.
       </p>
       <p>
         <strong>Depositum:</strong> kr.5.000 ved tilmelding. Restbeløbet senest
-        fire uger før afrejse.
+        otte uger før afrejse.
         <br />
         <strong>Indbetaling:</strong> Nordea, reg.nr. 2720, konto 4255 616 949.
         <br />
